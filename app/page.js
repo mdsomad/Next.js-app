0ffcd18ea8e78efcd244e91dad3bb15ff59149d8
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-11-19 20:55:51 +0530

    Api call complete

--- app/page.js
+++ app/page.js
@@ -1,14 +1,40 @@
 "use client";
-import React, { useState } from "react";
-import Header from "@/Components/Header";
-
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  const [user, setUser] = useState("Somad")
+  const [users, setusers] = useState([]);
+  const getUsers = async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(data);
+    setusers(data);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
-      <Header user={user}/>
-      {user}
+      <button
+        onClick={getUsers}
+        className="bg-green-600  text-white px-4 py-2 rounded font-bold"
+      >
+        Get Data
+      </button>
+      <div className="p-8 bg-slate-100 mt-5">
+        <ul>
+          {users.map((e) => {
+            return (
+              <li>
+                {e.username} --- <Link href={`/${e.id}`}>Exprole</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
